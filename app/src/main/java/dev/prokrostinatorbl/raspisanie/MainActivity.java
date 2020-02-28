692d791cb5de32996f946780c82c12782823e72a
Doctor156 <sumatoxin.m@yandex.ru>
2020-02-28 23:45:00 +0700

    покрасил кот

--- app/src/main/java/dev/prokrostinatorbl/raspisanie/MainActivity.java
+++ app/src/main/java/dev/prokrostinatorbl/raspisanie/MainActivity.java
@@ -1,15 +1,20 @@
 package dev.prokrostinatorbl.raspisanie;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import org.apache.commons.io.FileUtils;
-
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -19,8 +24,34 @@ import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    public int file_number = 80572;
 
-    public int file_number = 80500;
+    private static final int INTERNET_PERMISSION_CODE = 100;
+    private static final int STORAGE_PERMISSION_CODE = 101;
+
+    public void checkPermission(String permission, int requestCode)
+    {
+
+        // Checking if permission is not granted
+        if (ContextCompat.checkSelfPermission(
+                MainActivity.this,
+                permission)
+                == PackageManager.PERMISSION_DENIED) {
+            ActivityCompat
+                    .requestPermissions(
+                            MainActivity.this,
+                            new String[] { permission },
+                            requestCode);
+        }
+        else {
+            Toast
+                    .makeText(MainActivity.this,
+                            "Permission already granted",
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
 
 
     @Override
@@ -30,24 +61,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         Button aaa = (Button) findViewById(R.id.aaa);
         aaa.setOnClickListener(this);
-
     }
-
 
     @Override
     public void onClick(View view)
     {
-
-
         switch (view.getId())
         {
             case R.id.aaa:
+                checkPermission(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    STORAGE_PERMISSION_CODE);
+                checkPermission(
+                        Manifest.permission.INTERNET,
+                        INTERNET_PERMISSION_CODE);
+                checkPermission(
+                        Manifest.permission.READ_EXTERNAL_STORAGE,
+                        STORAGE_PERMISSION_CODE);
+                checkPermission(
+                        Manifest.permission.ACCESS_NETWORK_STATE,
+                        INTERNET_PERMISSION_CODE);
+                checkPermission(
+                        Manifest.permission.READ_PHONE_STATE,
+                        INTERNET_PERMISSION_CODE);
                 Downloader();
         }
-
-
     }
-
 
     private void Downloader(){
         String destFileName = "581-8_red.xls";
@@ -84,6 +123,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-
 }
-
